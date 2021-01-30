cf3e370ab4fc4c4ba15596933b568e4ac0debe53
TrungNguyen1208 <thanhtrungnguyen1208@gmail.com>
2021-01-30 14:41:16 +0700

    Add TodoListScreen

--- lib/day_two/TodoListScreen.dart
+++ lib/day_two/TodoListScreen.dart
@@ -9,12 +9,57 @@ class TodoListScreen extends StatefulWidget {
 
 class _TodoListScreenState extends State<TodoListScreen> {
   List<TodoModel> _list = todoList;
+  TextEditingController _controller = TextEditingController();
 
-  void _pressAddTodo() async {
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _addNewTodo(String todoName) {
     setState(() {
-      TodoModel model = TodoModel(title: "Create new project");
+      TodoModel model = TodoModel(title: todoName);
       _list.add(model);
     });
+  }
+
+  void _pressAddTodo() async {
+    await showDialog<String>(
+      context: context,
+      child: new AlertDialog(
+        contentPadding: const EdgeInsets.all(20.0),
+        content: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new TextField(
+                controller: _controller,
+                autofocus: true,
+                decoration: new InputDecoration(
+                  labelText: 'Create new task',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          new FlatButton(
+            child: const Text('OK'),
+            onPressed: () {
+              String todoName = _controller.text;
+              Navigator.pop(context);
+              _controller.text = '';
+              _addNewTodo(todoName);
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -34,11 +79,14 @@ class _TodoListScreenState extends State<TodoListScreen> {
           itemCount: _list.length,
           itemBuilder: (BuildContext context, int index) {
             TodoModel item = _list[index];
-            return TodoItem(item: item, onTapDone: (value) {
-              setState(() {
-                _list[index].isDone = value;
-              });
-            },);
+            return TodoItem(
+              item: item,
+              onTapDone: (value) {
+                setState(() {
+                  _list[index].isDone = value;
+                });
+              },
+            );
           },
         ),
       ),
@@ -60,37 +108,44 @@ class TodoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.grey, width: 0.5),
-        color:  Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 8,
-            blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Text(
-              item.title,
-              style: TextStyle(
-                fontSize: 17.0,
-                  color: item.isDone ? Colors.grey : Colors.black,
-                  decoration: item.isDone
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none),
+    return GestureDetector(
+      onTap: () => onTapDone(!item.isDone),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.grey, width: 0.5),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 8,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
             ),
-          ),
-          CircleCheckbox(value: item.isDone, onChanged: onTapDone, activeColor: Colors.green,)
-        ],
+          ],
+        ),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                item.title,
+                style: TextStyle(
+                    fontSize: 17.0,
+                    color: item.isDone ? Colors.grey : Colors.black,
+                    decoration: item.isDone
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ),
+            CircleCheckbox(
+              value: item.isDone,
+              onChanged: onTapDone,
+              activeColor: Colors.green,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -113,8 +168,7 @@ class CircleCheckbox extends StatelessWidget {
     this.activeColor,
     this.checkColor,
     this.materialTapTargetSize,
-  })
-      : assert(tristate != null),
+  })  : assert(tristate != null),
         assert(tristate || value != null),
         super(key: key);
 
@@ -128,12 +182,8 @@ class CircleCheckbox extends StatelessWidget {
           decoration: new BoxDecoration(
             border: Border.all(
                 width: 2,
-                color: Theme
-                    .of(context)
-                    .unselectedWidgetColor ??
-                    Theme
-                        .of(context)
-                        .disabledColor),
+                color: Theme.of(context).unselectedWidgetColor ??
+                    Theme.of(context).disabledColor),
             borderRadius: new BorderRadius.circular(100),
           ),
           child: Checkbox(
